9f286e60d5015a33134ae01dee700c580a3f2f98
Matthew Volk <volkmattj@gmail.com>
2019-08-09 07:54:00 -0700

    change sample data

--- api/routes/vendors.js
+++ api/routes/vendors.js
@@ -3,17 +3,17 @@ var router = express.Router()
 
 const data = {
   vendors: [
-    { id: 1, name: "Petr Datsko", rate: 65.00, slug: "petr-datsko", vendorName: "Petr Datsko" },
-    { id: 2, name: "Sergey Vlasov", rate: 65.00, slug: "sergey-vlasov", vendorName: "Sergey Vlasov" },
-    { id: 3, name: "Gaurav Shah", rate: 40.00, slug: "gaurav-shah", vendorName: "Gaurav Shah" },
-    { id: 4, name: "Jeet Rock", rate: 55.00, slug: "jeet-rock", vendorName: "Jeet Rock" },
-    { id: 5, name: "Mohammed Idrees", rate: 15.00, slug: "mohammed-idrees", vendorName: "Mohammed Idrees" },
+    { id: 1, name: "John Smith", rate: 65.00, slug: "petr-datsko", vendorName: "John Smith" },
+    { id: 2, name: "Jane Doe", rate: 65.00, slug: "sergey-vlasov", vendorName: "Jane Doe" },
+    { id: 3, name: "Raj Paul", rate: 40.00, slug: "gaurav-shah", vendorName: "Raj Paul" },
+    { id: 4, name: "Jan Smith", rate: 55.00, slug: "jan-smith", vendorName: "Jan Smith" },
+    { id: 5, name: "Mohammed Idrees", rate: 65.00, slug: "mohammed-idrees", vendorName: "Mohammed Idrees" },
     { id: 6, name: "Lila Gawrys", rate: 80.00, slug: "lila-gawrys", vendorName: "Lila Gawrys" },
     { id: 7, name: "Miko Batoon", rate: 60.00, slug: "miko-batoon", vendorName: "Miko Batoon" },
     { id: 8, name: "Jen Meredith", rate: 90.00, slug: "jen-meredith", vendorName: "Jen Meredith" },
     { id: 9, name: "Praveen Qahil", rate: 20.00, slug: "praveen-qahil", vendorName: "Praveen Qahil" },
     { id: 10, name: "Ankit Patel", rate: 35.00, slug: "ankit-patel", vendorName: "Ankit Patel" },
-    { id: 11, name: "Umair Obed", rate: 25.00, slug: "umair-obed", vendorName: "Umair Obed" },
+    { id: 11, name: "Jeet Rock", rate: 25.00, slug: "jeet-rock", vendorName: "Umair Obed" },
   ]
 }
 
