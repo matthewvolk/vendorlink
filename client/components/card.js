bac5581b85bd8399e9a64c0b8345efe1d2c1679d
Matthew Volk <volkmattj@gmail.com>
2019-09-05 16:14:18 -0700

    remove unordered list and replace with Card component

--- client/components/card.js
+++ client/components/card.js
@@ -10,12 +10,112 @@ class Card extends Component {
     return (
       <>
         <div className="card">
-          Hello, World!
+          <div className="arrows">
+            <div className="container">
+              <div className="arrow up blue">&#10140;</div>
+              <div className="count">0</div>
+              <div className="arrow down red">&#10140;</div>
+            </div>
+          </div>
+          <Link as={`/vendor/${this.props.slug}`} href={`/vendor?id=${this.props.slug}`}>
+            <div className="content">
+              <h3 className="title">{this.props.name}</h3>
+              <p className="detail">Hourly Rate: ${this.props.rate}</p>
+            </div>
+          </Link>
         </div>
 
         <style jsx>{`
           .card {
-            border: 1px solid gray;
+            border: 1px solid #C5C5C5;
+            margin-bottom: -1px;
+            display: flex;
+            background-color: #FDFDFE;
+            cursor: pointer;
+          }
+
+          .arrows {
+            display: flex;
+            flex-direction: column;
+            background-color: #F7F8F9;
+          }
+
+          .container {
+            padding: 15px 7.5px;
+            background-color: #F7F8F9;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            min-width: 1.5rem;
+            max-width: 1.5rem;
+          }
+
+          .arrow {
+            font-weight: 700;
+            cursor: pointer;
+            color: #76797B;
+            padding: 2.5px;
+          }
+
+          .blue:hover {
+            color: #1B8ACE;
+          }
+
+          .red:hover {
+            color: #FF0048;
+          }
+
+          .arrow:hover {
+            background-color: #e2e2e2;
+            border-radius: 5px;
+          }
+
+          .up {
+            transform: rotate(-90deg);
+            -webkit-transform: rotate(-90deg);
+          }
+
+          .down {
+            transform: rotate(90deg);
+            -webkit-transform: rotate(90deg);
+          }
+
+          .count {
+            font-weight: 700;
+            font-size: 0.75rem;
+            margin: 10px 5px;
+            color: #1a1a1b;
+          }
+
+          .title {
+            font-size: 1.25rem;
+            margin: 0;
+            padding: 10px 10px 10px 10px;
+            margin-top: 5px;
+            font-weight: 600;
+            cursor: pointer;
+          }
+
+          .detail {
+            font-size: 0.9rem;
+            margin: 0;
+            padding: 0 10px;
+            color: #7c7c7c;
+          }
+
+          .secondary-link {
+            font-size: 0.9rem; 
+            color: rgb(124, 124, 124); 
+            text-decoration: none;
+            padding: 5px 7.5px;
+            font-weight: 700;
+          }
+      
+          .secondary-link:hover,
+          .secondary-link:active {
+            background-color: #f2f2f2;
+            border-radius: 5px;
           }
         `}</style>
       </>
--- client/pages/index.js
+++ client/pages/index.js
@@ -13,8 +13,7 @@ const Index = ({ vendors }) => (
       <div style={{borderBottom: "1px solid #EAECEF"}}>
         <div className="container">
           <h1 className="hero-text">Hire Better Freelancers.</h1>
-          <p style={{marginBottom: "0.5em"}} className="tagline">Meet &amp; hire amazing talent from our invite-only community of elite web developers, designers, and marketers.</p>
-          <p style={{fontSize: "0.85rem", marginTop: "0.5em", marginBottom: "2em"}}>*Disclaimer: This website is in development, and not yet fully functional. Feel free to browse around in the meantime!</p>
+          <p style={{margin: "1em 0 2em 0"}} className="tagline">Meet &amp; hire amazing talent from our invite-only community of elite web developers, designers, and marketers. *Note: This website is in development, and not yet fully functional. Feel free to browse around in the meantime!</p>
           <Link href={`/our-process`}>
             <a>What makes us different?</a>
           </Link>
@@ -30,25 +29,12 @@ const Index = ({ vendors }) => (
       </div>
 
       <div className="background">
-        <div className="container-cards" style={{maxWidth: "1120px"}}>
+        <div className="container-cards" style={{maxWidth: "1180px"}}>
           {/* <p style={{color: "red"}}>@todo Add sort bar</p> */}
 
-
-
-          <ul>
-            {vendors.map(vendor => (
-              <li key={vendor.slug}>
-                <Link as={`/vendor/${vendor.slug}`} href={`/vendor?id=${vendor.slug}`}>
-                  <h3 style={{marginLeft: "6.75px"}}>{vendor.name}</h3>
-                </Link>
-                  <p className="card-detail" style={{marginLeft: "6.75px"}}>Hourly Rate: ${vendor.rate.toFixed(2)}/hr</p> 
-                  <a className="secondary-link" href="/">Share</a> 
-                  <a className="secondary-link" href="/">Save</a> 
-                  <a className="secondary-link" href="/">Hide</a> 
-                  <a className="secondary-link" href="/">Report</a>
-              </li>
-            ))}
-          </ul>
+          {vendors.map(vendor => (
+            <Card name={vendor.name} rate={vendor.rate.toFixed(2)} slug={vendor.slug} />
+          ))}
         </div>
       </div>
       
@@ -67,37 +53,8 @@ const Index = ({ vendors }) => (
       padding-bottom: 0.5rem;
     }
 
-    ul {
-      list-style-type: none;
-      margin: 0;
-      padding: 0;
-    }
-
-    li {
-      padding: 1rem;
-      border-top: 1px solid #C5C5C5;
-      border-right: 1px solid #C5C5C5;
-      border-bottom: 1px solid #C5C5C5;
-      border-left: 1px solid #C5C5C5;
-      background-color: #FDFDFE;
-      margin-bottom: -1px;
-    }
-
-    h3 {
-      cursor: pointer;
-      margin: 0;
-      font-weight: 600;
-    }
-
     .tagline {
       font-weight: 300;
-    }
-
-    .card-detail {
-      font-size: 0.9rem;
-      margin-bottom: 1rem;
-      margin-top: 0.35rem;
-      color: rgb(124, 124, 124);
     }
 
     .sort-links > a {
@@ -111,20 +68,6 @@ const Index = ({ vendors }) => (
 
     .sort-links > a:hover,
     .sort-links > a:active {
-      background-color: #f2f2f2;
-      border-radius: 5px;
-    }
-
-    .secondary-link {
-      font-size: 0.9rem; 
-      color: rgb(124, 124, 124); 
-      text-decoration: none;
-      padding: 5px 7.5px;
-      font-weight: 700;
-    }
-
-    .secondary-link:hover,
-    .secondary-link:active {
       background-color: #f2f2f2;
       border-radius: 5px;
     }
