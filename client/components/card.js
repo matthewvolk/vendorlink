59ff067aaf3ff647e55bc77ff5774a294b88cef1
Matthew Volk <volkmattj@gmail.com>
2019-09-05 17:16:29 -0700

    change arrow color

--- client/components/card.js
+++ client/components/card.js
@@ -14,7 +14,7 @@ class Card extends Component {
             <div className="container">
               <div className="arrow up blue">&#10140;</div>
               <div className="count">0</div>
-              <div className="arrow down red">&#10140;</div>
+              <div className="arrow down blue">&#10140;</div>
             </div>
           </div>
           <Link as={`/vendor/${this.props.slug}`} href={`/vendor?id=${this.props.slug}`}>
@@ -59,7 +59,7 @@ class Card extends Component {
           }
 
           .blue:hover {
-            color: #00D07C;
+            color: #2085C4;
           }
 
           .red:hover {
