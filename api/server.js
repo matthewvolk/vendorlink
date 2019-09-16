03dba573ea097ad10ba0a01d8164b93fe2d8bec5
Matthew Volk <volkmattj@gmail.com>
2019-09-16 15:03:00 -0700

    rename sequelize config file

--- api/server.js
+++ api/server.js
@@ -12,12 +12,7 @@ const auth = require('./routes/auth')
  * @todo fix CORS for production, only allow Next.js client for
  * dev, production, and maybe an admin dashboard for coupons etc.
  */
-
-app.use(cors({
-  origin: "*",
-  allowedHeaders: "*"
-}));
-
+app.use(cors({ origin: "*", allowedHeaders: "*" }));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
@@ -35,9 +30,6 @@ sequelize
 app.use('/api/v1/checkout', checkout)
 app.use('/api/v1/vendors', vendors)
 app.use('/api/v1/auth', auth)
-
-app.get('/', (req, res) => res.json({
-  "version": 1
-}))
+app.get('/', (req, res) => res.json({ "version": 1 }))
 
 app.listen(port, () => console.log(`\n> VendorLink API listening on port http://localhost:${port}!`))
